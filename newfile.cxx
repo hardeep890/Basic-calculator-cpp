7698fce042b0c18d323146d25804e00e4d23e7ae
hardeep890 <harrysh890@gmail.com>
2025-07-31 21:06:24 +0530

    Update newfile.cxx

--- newfile.cxx
+++ newfile.cxx
@@ -9,7 +9,11 @@ int main()
     cin>>b;
     cout<<"Addition  "<<a+b<<endl;
     cout<<"subtraction  "<<a-b<<endl;
-    cout<<"Division  "<<a/b<<endl;
+    if (b != 0)
+        cout << "Division: " << a / b << endl;
+    else
+        cout << "Division not possible (b = 0)" << endl;
+    
     cout<<"product  "<<a*b<<endl;
     return 0;
 }
