2b23dfad0281cec3869d803d088231b53cdf2c4f
hardeep890 <harrysh890@gmail.com>
2025-07-31 20:58:06 +0530

    Added newfile.cxx for arithmetic operations

---
+++ newfile.cxx
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the first value : ";
+    cin>>a;
+    cout<<"enetr the second  value : ";
+    cin>>b;
+    cout<<"Addition  "<<a+b<<endl;
+    cout<<"subtraction  "<<a-b<<endl;
+    cout<<"Division  "<<a/b<<endl;
+    cout<<"product  "<<a*b<<endl;
+    return 0;
+}
